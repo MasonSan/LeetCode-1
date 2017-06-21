4ea3e01ac3ec4fca71503fe9cd99cbe18629ce58
Mason Cheng <dada5109@gmail.com>
2017-06-21 12:57:37 +0800

    initialize project

---
+++ Two_Sum/Two_Sum/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+        }
+    }
+}
